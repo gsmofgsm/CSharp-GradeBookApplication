74f3b6875b9c8d5dd601f51be279c86dabd6bd8a
Qing Ma <gsmofgsm@hotmail.com>
2020-09-15 16:54:33 +0200

    StandardGradeBook Type is Standard - 2.4

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -6,6 +7,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
+            Type = GradeBookType.Standard;
         }
     }
 }
