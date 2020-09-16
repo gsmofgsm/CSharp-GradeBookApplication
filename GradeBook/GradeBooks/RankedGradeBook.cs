9622f0c7c384b10b46444fa4883b156eae158e11
Qing Ma <gsmofgsm@hotmail.com>
2020-09-16 13:54:13 +0200

    GetLetterGrade - 3.2-3.5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,9 +12,40 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
+            var count = Students.Count;
+            if (count < 5)
             {
                 throw new InvalidOperationException();
+            }
+
+            Students.Sort((x, y) => y.AverageGrade.CompareTo(x.AverageGrade));
+
+            int pos = 0;
+            for (var i = 0; i < count; i++)
+            {
+                if (Students[i].AverageGrade < averageGrade)
+                {
+                    pos = i;
+                    break;
+                }
+            }
+
+            var percentage = pos / count;
+            if (percentage <= 0.20)
+            {
+                return 'A';
+            }
+            else if (percentage <= 0.40)
+            {
+                return 'B';
+            }
+            else if (percentage <= 0.60)
+            {
+                return 'C';
+            }
+            else if (percentage <= 0.80)
+            {
+                return 'D';
             }
             return 'F';
         }
